b2774e64b4bb3b691cd94b2511abf3d952a694b3
Cableties <38928673+Cableties@users.noreply.github.com>
2023-03-21 01:33:50 +0000

    Task 3.1

--- 2.1/app.js
+++ 2.1/app.js
@@ -1,19 +1,47 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
-const addTwoNumber = (n1, n2) => {
-	return n1 + n2;
-};
+const bodyParser = require("body-parser");
+let jsonParser = bodyParser.json();
+let users = [
+	{ id: 1, name: "John" },
+	{ id: 2, name: "Mary" },
+	{ id: 3, name: "Peter" },
+];
 
-app.get("/addTwoNumber", (req, res) => {
-	const n1 = parseInt(req.query.n1);
-	const n2 = parseInt(req.query.n2);
-	const result = addTwoNumber(n1, n2);
-	res.json({ statuscode: 200, data: result });
+app.get("/users", (req, res) => {
+	res.json(users);
 });
 
-console.log(addTwoNumber(19, 12));
-const port = 3040;
+app.get("/users/:id", (req, res) => {
+	let id = +req.params.id;
+	console.log(id);
+	let user = users.find((u) => u.id === id);
+	console.log(user);
+	res.send(user);
+});
+
+app.post("/users", jsonParser, (req, res) => {
+	const newUser = req.body;
+	users.push(newUser);
+	res.status(201).json(newUser);
+});
+
+app.put("/users/:id", jsonParser, (req, res) => {
+	const userId = parseInt(req.params.id);
+	const updateedUser = req.body;
+	users = users.map((user) => (user.id === userId ? updateedUser : user));
+	res.status(200).json(updateedUser);
+});
+
+app.delete("/users/:id", (req, res) => {
+	const userId = parseInt(req.params.id);
+	users = users.filter((user) => user.id !== userId);
+	res.status(204).send();
+});
+
+console.log(users);
+const port = 3000;
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
